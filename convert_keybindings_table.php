8e7d48ec3ccfbe1fe1a9b1100d6ee629e25c5f50
SoftMoon-WebWare <github.account@softmoon-webware.com>
2014-03-09 14:57:45 -0400

    Update convert_keybindings_table.php
    Now fully supports MSIE
    Now supports Greek keyboards
    Now supports unlimited keybinding categories

--- convert_keybindings_table.php
+++ convert_keybindings_table.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html><!-- HTML5  -->
+<html><!--  HTML5  -->
 <head>
 <meta charset='UTF-8'>
-<meta name='x-generator' content="SoftMoon-WebWare's convert_keybindings_table.php">
+<meta name='x-generator' content="SoftMoon-WebWare's convert_keybindings_table.php  http://softmoon-webware.com/Komodo/">
 <title>Komodo keybindings by key</title>
-<style>
+<style type='text/css' media='screen,projection'>
 body {
 	color: black;
 	background-color: white;
 	padding-top: 2em; }
 header {
 	margin-left: 2.618em; }
-header p {
+header div {
 	display: none; }
 header h1 {
 	margin-left: 1em; }
@@ -61,9 +61,11 @@ aside + aside:hover {
 	overflow: auto;
 	padding-right: .618em; }
 aside + aside h4 {
-	position: absolute;
-	top: 3.382em;
+	position: fixed;
+	top: 4.618em; /*3.382em;*/
 	left: -2.82em;
+	margin: 0;
+	padding: 0;
 	transform: rotate(270deg);
 	-webkit-transform: rotate(270deg);
 	-ms-transform: rotate(270deg);
@@ -85,7 +87,7 @@ kbd span {
 	font-weight: bold;
 	text-align: center; /* ignored when in a justified <p> */
 	margin: 0 .27em;
-	padding: 0 .618em;  }
+	padding: 0 .618em; }
 .tr {
 	margin: 1.618em;
 	border-bottom: 1px solid; }
@@ -94,8 +96,6 @@ kbd.th span {
 	color: yellow;
 	margin: 1em 0 .618em; }
 .td {
-	margin-right: 4em;
-	position: relative;
 	border-bottom: 1px dotted gray; }
 .td kbd,
 .td > span:first-child,
@@ -182,7 +182,7 @@ body.flow .window_pane {
 	position: static;
 	width: 100%;
 	height: auto; }
-body.sideby .window_pane { /*can confuse MSIE*/
+body.sideby .window_pane { /*can confuse MSIE if aside-nav is before the main sections*/
 	position: fixed;
 	top: 1.2em;
 	left: 50%;
@@ -190,10 +190,10 @@ body.sideby .window_pane { /*can confuse MSIE*/
 	bottom: 0;
 	width: 50%;
 	overflow: auto; }
-body.sideby aside + .window_pane {
+body.sideby header + .window_pane {
 	left: 0;
 	border-right: 2px double; }
-body.stack .window_pane { /*can confuse MSIE*/
+body.stack .window_pane { /*can confuse MSIE if aside-nav is before the main sections*/
 	position: fixed;
 	top: 50%; /*for Presto*/
 	top: calc(50% + .6em);
@@ -202,7 +202,7 @@ body.stack .window_pane { /*can confuse MSIE*/
 	bottom: 0;
 	width: 100%;
 	overflow: auto; }
-body.stack aside + .window_pane {
+body.stack header + .window_pane {
 	top: 1.2em;
 	bottom: 50%; /*for Presto*/
 	bottom: calc(50% - .6em);
@@ -221,12 +221,14 @@ body.stack header {
 	background-color: white;
 	border-right: 2px double;
 	z-index: 6180; }
-body.sideby header p,
-body.stack header p {
+body.sideby header div,
+body.stack header div {
 	display: block;
 	position: absolute;
-	top: .382em;
+	top: 2em;
 	left: -1.382em;
+	margin: 0;
+	padding: 0;
 	transform: rotate(270deg);
 	-webkit-transform: rotate(270deg);
 	-ms-transform: rotate(270deg);
@@ -238,7 +240,7 @@ body.stack header:hover {
 	height: auto;
 	border: 2px double; }
 </style>
-<script>
+<script type='text/javascript'>
 function setPageLayout(layout)  {
 	document.body.className=layout;  }
 function show(inp)  {
@@ -268,8 +270,8 @@ function show(inp)  {
 <meta name='author' content='Joe Golembieski, SoftMoon-WebWare' />
 <meta name='contact' content='http://softmoon-webware.com/Komodo/' />
 <meta name='copyright' content='Copyright © 2014 Joe Golembieski, SoftMoon-WebWare' />
-<meta name='license' content='free to use, distribute, modify; original author and copyright must remain intact' />
-<meta name='last_update' content='March 2, 2014' />
+<meta name='license' content='free to use, distribute, modify; original author, copyright, and license must remain intact' />
+<meta name='last_update' content='March 9, 2014' />
 
 	Komodo Edit >> help >> list key bindings  (this will open a browser window showing key bindings by category)
 	Generally, right-click on the page >> “save as file” |or| “view source” >> copy&paste to a text-editor and save
@@ -294,17 +296,22 @@ function show(inp)  {
 	http://localhost/convert_keybindings_table.php?filename=JuliesKBs&compare=TomsKBs&filter=snippets,macros
 	http://localhost/convert_keybindings_table.php?filename=JuliesKBs&compare=TomsKBs&filter=!snippets,!macros,!editor,snippets1,editor2
 	http://localhost/convert_keybindings_table.php?filename=JuliesKBs&compare=TomsKBs&filter=!snippets2,!macros,!editor1  ←same as line above, but if there is more than two files, inclusion/exclusion becomes more specific. As noted below, more than two requires a different browser-script interface.
-	http://localhost/convert_keybindings_table.php?groomcats=true   ←shows keybindings by category listed after keybindings by key
+	http://localhost/convert_keybindings_table.php?groomcats=false  ←does not show keybindings by category
 	http://localhost/convert_keybindings_table.php?groomcats=first  ←shows keybindings by category listed before keybindings by key
 	http://localhost/convert_keybindings_table.php?filename=JuliesKBsnippets&compare=TomsKBmacros&filter=snippets1,macros2&groomcats=first
 */
+
 //  ¡! DO NOT PUT THIS ON A PUBLIC SERVER !¡  ($_GET requests need to be filtered first!)  \\
+
+$myKeybindings=new KomodoKeybindingSupport(strToLower($_GET['groomcats'])!=='false');
+
 if (isset($_GET['filename']))  {
 	if (is_file("Komodo Keybindings [".$_GET['filename']."].htm"))
 		$_GET['filename']="Komodo Keybindings [".$_GET['filename']."].htm";  }
-else  $_GET['filename']='keybindings_by_category.htm';
-$myKeybindings=new KomodoKeybindingSupport($_GET['filename']);  //filename of saved HTML generated by Komodo
-$myKeybindings->parse_cat_file($_GET['filter'], isset($_GET['groomcats']));
+else  $_GET['filename']='keybindings_by_category.htm';   // filename of user-saved HTML generated by Komodo
+
+$myKeybindings->parse_cat_file($_GET['filename'], $_GET['filter']);
+
 if (isset($_GET['compare']))  {
 	if (is_file("Komodo Keybindings [".$_GET['compare']."].htm"))
 		$_GET['compare']="Komodo Keybindings [".$_GET['compare']."].htm";
@@ -312,11 +319,10 @@ if (isset($_GET['compare']))  {
 		$_GET['compare']='Komodo Keybindings [Default].htm';
 	//NOTE we can compare as many cat_files as you like by repeating this method below…
 	// …but $_GET format herein only allows one compare and CSS given above only handles up to 7…
-	$myKeybindings->cat_file=file_get_contents($_GET['compare']);
-	$myKeybindings->parse_cat_file($_GET['filter'], isset($_GET['groomcats']));  }
+	$myKeybindings->parse_cat_file($_GET['compare'], $_GET['filter']);  }
+
 $myKeybindings->sort();
 $myKeybindings->build_HTML();
-if (isset($_GET['groomcats']))  $myKeybindings->groom_cats();
 $myKeybindings->echo_HTML(strToLower($_GET['groomcats'])==='first');
 
 
@@ -325,35 +331,33 @@ class KomodoKeybindingSupport  {
 
 public $cat_file, $eol, $charset,
 			 $headline=array(), $bound_keys=array(), $cats=array(), $cat_list=array(),
-			 $snippets=0, $macros=0, $HTML=array('keys'=>"", 'cats'=>""),
-			 $cat_colors=array("#202000", "#002000", "#002020", "#000020", "#200020", "#002000",
-												 "#002020", "#202000", "#002000", "#002020", "#000020", "#200020", ),
-			 $cat_backcolors=array("#DFDFFF", "#FFDFFF", "#FFDFDF", "#FFFFDF", "#DFFFDF", "#DFFFFF",
-														 "#AFAFFF", "#FFAFFF", "#FFAFAF", "#FFFFAF", "#AFFFAF", "#AFFFFF" );
-private $filecount=0, $order_marks="/α|β|γ|δ|ε|ζ|η|θ|ι|κ|λ|μ|ν/u",  //note the PHP-specific “u” modifier for UTF-8 processing
-				$doubles=array('α1 !', 'α2 @', 'α3 #', 'α4 $', 'α5 %', 'α6 ^', 'α7 &', 'α8 *', 'α9 (', 'β0 )', 'γ- _', 'δ= +',
-											 'ε[ {', 'ζ] }', 'η\\ |', 'θ` ~', 'ι\' "', 'κ; :', 'λ, <', 'μ. >', 'ν/ ?');
-/* Notes: (1) The $doubles array above is for a standard American-style keyboard used with Windows®.
+			 $snippets=0, $macros=0, $URLs=0, $commands=0,
+			 $HTML=array('keys'=>"", 'cats'=>"");
+private $¿groom_cats, $filecount=0,
+				$order_marks="/^[αβγδεζηθικλμν•]+•/u",  //note the PHP-specific “u” modifier for UTF-8 processing
+				$doubles=array('α•1 !', 'α•2 @', 'α•3 #', 'α•4 $', 'α•5 %', 'α•6 ^', 'α•7 &', 'α•8 *', 'α•9 (', 'β•0 )',
+											 'γ•- _', 'δ•= +', 'ε•[ {', 'ζ•] }', 'η•\\ |', 'θ•` ~', 'ι•\' "', 'κ•; :', 'λ•, <', 'μ•. >', 'ν•/ ?');
+/* Notes: (1) The $doubles array above is for a standard American-style keyboard used with MS-Windows®.
  * I am ignorant of other keyboard layouts so you may need to change it to match your keyboard.
  * The same applies to keys such as “backspace” or “page_up” etc.: code may need to be modified below.
  * (2) Notice keys are prefixed with Greek to facilitate sorting them to a specific display preference.
- * The “build_HTML” method specifically removes Greek glyphs from keys, so if you are in Greece and your
- * keyboard has greek glyphs on it (I really am ignorant, do you have that in Greece?) you will need to
- * reconfigure this code to use a different set of glyphs from another language.  As long as no key has
- * a latin character(A-Z) on your Greek keyboard, you could then just use A-Z instead of Greek α-ν for “order-marks.”
+ * The “build_HTML” method specifically removes these Greek glyphs from keys when found at the beginning
+ * of a key and when postfixed with a bullet • (see $order_marks above).  This should be compatible with a
+ * Greek-keyboard layout, provided none of the Greek-keys use the bullet character.
  */
 
-public function __construct($cat_file)  {$this->cat_file=file_get_contents($cat_file);}
+public function __construct($¿groom_cats)  {$this->¿groom_cats=$¿groom_cats;}
 
 
-public function parse_cat_file($filter, $¿collect_cats)  { $this->filecount++;
+public function parse_cat_file($cat_file, $filter)  { $this->filecount++;
+	$this->cat_file=file_get_contents($cat_file);
 	preg_match('/\n\r|\n/', $this->cat_file, $eol);
 	preg_match('/<meta [^>]*charset=[\'"]?([-_a-z0-9]+)/i', $this->cat_file, $charset);
 	$this->eol=$eol[0];  $this->charset=$charset[1];
 	$F=explode($eol[0], iconv($charset[1], 'UTF-8', $this->cat_file));
 
 	if ($filter)  $filter=explode(',', strToLower($filter));
-	if (!$filter or count(preg_grep('/^!/',  $filter))===0)  $avoids=FALSE;
+	if (!$filter  or  count(preg_grep('/^!/',  $filter))===0)  $avoids=FALSE;
 	else  {
 		$avoids[0]=preg_grep("/^![^1-9]+$/", $filter);  if (count($avoids[0])===0)  $avoids[0]=FALSE;
 		$avoids[1]=preg_grep("/^![^1-9]+".$this->filecount."$/", $filter);  if (count($avoids[1])===0)  $avoids[1]=FALSE;
@@ -379,7 +383,7 @@ public function parse_cat_file($filter, $¿collect_cats)  { $this->filecount++;
 		while (preg_match('/<p>([^<]+)<\/p>/u', $F[$i], $m, PREG_OFFSET_CAPTURE, $o))  {
 			$o=$m[1][1]+strLen($m[1][0]);
 			if ($binding)  {
-				if ($¿collect_cats)  $this->augment_cats($cat, $binding[0], $m[1][0]);
+				if ($this->¿groom_cats)  $this->augment_cats($cat, $binding[0], $m[1][0]);
 				$m[1][0]=array_map('trim', explode(', ', $m[1][0]));  }
 			else  $binding=array();
 			array_push($binding, $m[1][0]);  }
@@ -417,33 +421,33 @@ private function augment_cats($cat, $description, $binding)  {
 public function get_mod($keystroke)  {
 	return (stripos($keystroke, 'shift+')!==FALSE ? 1 : 0)
 				+(stripos($keystroke, 'ctrl+')!==FALSE ? 2 : 0)
-				+(stripos($keystroke, 'alt+')!==FALSE ? 4 : 0);   }
+				+(stripos($keystroke, 'alt+')!==FALSE ? 4 : 0);  }
 
 public function get_key($keystroke)  {
 	$key=html_entity_decode(preg_replace('/(shift|ctrl|alt)\+/iu', "", $keystroke));
 	switch (strToLower($key))  { //group similar keys for sort
-		case "up": $key="αα↑ ".$key;  break;
-		case "down": $key="αβ↓ ".$key;  break;
-		case "left": $key="αγ← ".$key;  break;
-		case "right": $key="αδ→ ".$key;  break;
-		case "page_up": $key="βα▲ ".$key;  break;
-		case "page_down": $key="ββ▼ ".$key;  break;
-		case "home": $key="βγ◄ ".$key;  break;
-		case "end": $key="βδ► ".$key;  break;
+		case "up": $key="αα•↑ ".$key;  break;
+		case "down": $key="αβ•↓ ".$key;  break;
+		case "left": $key="αγ•← ".$key;  break;
+		case "right": $key="αδ•→ ".$key;  break;
+		case "page_up": $key="βα•▲ ".$key;  break;
+		case "page_down": $key="ββ•▼ ".$key;  break;
+		case "home": $key="βγ•◄ ".$key;  break;
+		case "end": $key="βδ•► ".$key;  break;
 		case "backspace":
 		case "delete":
-		case "insert": $key="γ".$key;  break;  }
+		case "insert": $key="γ•".$key;  break;  }
 	//Note how keys are prefixed with Greek specifically for sorting: αβγδ
-	if (strLen($key)>1)  $key="α".$key;
-	else if ($key==='/')  $key='γν/ ?';
-	else if ($key==='\\')  $key='γη\\ |';
-	else if ($key==='^')  $key='γα6 ^';
+	if (strLen($key)>1)  $key="α•".$key;
+	else if ($key==='/')  $key='γν•/ ?';
+	else if ($key==='\\')  $key='γη•\\ |';
+	else if ($key==='^')  $key='γα•6 ^';
 	else if (($nukey=array_merge(preg_grep("/[$key]/u", $this->doubles))) and $nukey[0])  $key="γ".$nukey[0];
-	else $key="β".$key;
+	else $key="β•".$key;
 	return $key;  }
 
 
-public function get_key_ref($key)   {
+public function get_key_ref($key)  {
 	preg_match("/^[^ ]+/u", $key, $m);  $key=$m[0];
 	switch ($key)  {
 		case "`": return "key_grave";
@@ -488,6 +492,7 @@ private function check_combo_keyclash($a, $b)  {
 public function sort()  {ksort($this->bound_keys, SORT_NATURAL|SORT_FLAG_CASE);}
 
 
+
 public function build_HTML()  {
 	$mod=array('shift', 'ctrl', 'alt');  $html=&$this->HTML['keys'];
 	$html.="\n<section id='show_keys' class='window_pane'><h2>Key Bindings by key:</h2>\n\n";
@@ -507,6 +512,8 @@ public function build_HTML()  {
 				if (!isset($bindings[$i][$j]))  continue;
 				if ($bindings[$i][$j]['class']==='Snippets')  $this->snippets++;
 				if ($bindings[$i][$j]['class']==='Macros')  $this->macros++;
+				if ($bindings[$i][$j]['class']==='URLs')  $this->URLs++;
+				if ($bindings[$i][$j]['class']==='Commands')  $this->commands++;
 				$combo="";
 				if ($bindings[$i][$j]['combo'])  {
 					for ($k=0, $c=count($bindings[$i][$j]['combo']);  $k<$c;  $k++)  {
@@ -523,10 +530,10 @@ public function build_HTML()  {
 				$html.="</span> ".$bindings[$i][$j]['description']."</li>\n";  }
 			$html.="</ul></div>\n";  }
 		$html.="</div>\n\n";  }
-	$html.="</section><!--  close #show_keys  -->\n\n";  }
+	$html.="</section><!--  close #show_keys  -->\n\n";
 
+	if (!$this->¿groom_cats)  return;
 
-public function groom_cats()  {
 	$html=&$this->HTML['cats'];
 	$html.="\n<section id='show_cats' class='window_pane'><h2>Key Bindings by category:</h2>\n\n";
 	foreach ($this->cats  as  $cat=>$bindings)  {
@@ -564,15 +571,28 @@ public function groom_cats()  {
 	$html.="</section><!--  close #show_cats  -->\n\n";  }
 
 
-public function echo_HTML($¿cats_first)  {
-	$headline="<header>\n<p>˅ Files: ˅</p>\n";
+
+public function echo_HTML($¿cats_first)  {  ?>
+<header>
+<div>˅ Files: ˅</div>
+<?php
 	for ($i=1, $c=count($this->headline);  $i<=$c;  $i++)  {
-		$headline.="<h1>".($this->filecount>1 ? "<span class='file$i'>$i</span>" : "")
-							.$this->headline[$i-1]."</h1>\n";  }
-	echo $headline,
-			 $this->snippets," total snippet keybindings<br>\n",
-			 $this->macros," total macro keybindings<br>\n",
-			 "</header>\n<aside id='nav'><h4>▼↓ Navigation: on this page ↓▼</h4>\n<nav>\n";
+		echo "<h1>",($this->filecount>1 ? "<span class='file$i'>$i</span>" : ""),$this->headline[$i-1]."</h1>\n";  }
+	echo "<p>",$this->snippets," total snippet keybindings</p>\n",
+			 "<p>",$this->macros," total macro keybindings</p>\n",
+			 "<p>",$this->URLs," total URL keybindings</p>\n",
+			 "<p>",$this->commands," total command keybindings</p>\n";  ?>
+</header>
+
+<?php
+	if ($¿cats_first)
+		echo $this->HTML['cats'], $this->HTML['keys'];
+	else
+		echo $this->HTML['keys'], $this->HTML['cats'];  ?>
+
+<aside id='nav'><h4>▼↓ Navigation: on this page ↓▼</h4>
+<nav>
+<?php
 	foreach ($this->bound_keys as $key=>$dummy)  {
 		$key=htmlSpecialChars(preg_replace($this->order_marks, "", $key));
 		$nav['key'].="<li><a href='#".$this->get_key_ref($key)."'>$key</a></li>\n";  }
@@ -581,8 +601,7 @@ public function echo_HTML($¿cats_first)  {
 	if ($¿cats_first)
 		echo $nav['cat'], "</nav>\n<nav>\n", $nav['key'];
 	else
-		echo $nav['key'], "</nav>\n<nav>\n", $nav['cat'];
-?>
+		echo $nav['key'], "</nav>\n<nav>\n", $nav['cat'];  ?>
 </nav>
 </aside>
 <aside><h4>˅ Page Layout ˅</h4>
@@ -598,17 +617,41 @@ public function echo_HTML($¿cats_first)  {
 	<label><input type='radio' name='<?php echo $this->cat_list[$i]; ?>' value='hide' onchange="show(this);" />hide</label>
 	<label><input type='radio' name='<?php echo $this->cat_list[$i]; ?>' value='show' onchange="show(this);" checked />show</label>
 	<label><input type='radio' name='<?php echo $this->cat_list[$i]; ?>' value='highlight' onclick="show(this)" />highlight</label>
-	<input type='color' value='<?php echo $this->cat_colors[$i]; ?>' />
-	<input type='color' value='<?php echo $this->cat_backcolors[$i]; ?>' />
+	<input type='color' value='<?php echo "#",$this->rgb_from_hcg($i/$c+.5, .17, 0); ?>' />
+	<input type='color' value='<?php echo "#",$this->rgb_from_hcg($i/$c, .22, 1); ?>' />
 </fieldset>
-<?php } //close loop ?>
+<?php  } //close loop ?>
 </aside>
-<?php
-	if ($¿cats_first)
-		echo $this->HTML['cats'], $this->HTML['keys'];
-	else
-		echo $this->HTML['keys'], $this->HTML['cats'];  }
+<?php  } //close  echo_HTML  method
 
-}  // close KomodoSupport class
+
+public function rgb_from_hcg($hue, $chroma, $gray)  {
+	// hue (pure color from color wheel)
+	// chroma (how much color to mix into the gray: 0=no color, pure gray …… 1=pure color, no gray)
+	// gray (grayscale: 0=black …… 1=white)
+	//HCG values from 0 to 1
+	//RGB results from 000000 to FFFFFF
+	if ( $chroma == 0 )  return str_repeat($this->decByte_2hex($gray*255), 3);
+	$hue = fmod($hue, 1) * 6;
+	$x = fmod($hue, 1);
+	if ($hue<1)  {$red=1; $grn=$x; $blu=0;}
+	else
+	if ($hue<2)  {$red=1-$x; $grn=1; $blu=0;}
+	else
+	if ($hue<3)  {$red=0; $grn=1; $blu=$x;}
+	else
+	if ($hue<4)  {$red=0; $grn=1-$x; $blu=1;}
+	else
+	if ($hue<5)  {$red=$x; $grn=0; $blu=1;}
+	else
+	if ($hue<6)  {$red=1; $grn=0; $blu=1-$x;}
+	$red=$red+($gray-$red)*(1-$chroma);
+	$grn=$grn+($gray-$grn)*(1-$chroma);
+	$blu=$blu+($gray-$blu)*(1-$chroma);
+	return $this->decByte_2hex($red*255).$this->decByte_2hex($grn*255).$this->decByte_2hex($blu*255);  }
+
+public function decByte_2hex($d)  {return strToUpper(($d<16 ? '0' : "").dechex($d));}
+
+}  // close  KomodoKeybindingSupport  class
 ?>
 </body></html>
